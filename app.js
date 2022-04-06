de7cf0d5c0901b849eba77b3c0a4a38e9f9e50b9
Andymanes <101294232+Andymanes@users.noreply.github.com>
2022-04-06 16:10:02 -0400

    deck variable properly set up

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+// suits array
+let suits = ['S', 'C', 'H', 'D']
+// console.log(suits)
 
+let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+// console.log(ranks)
+let deck = []
 
+// for (let i = 0; i < ranks.length; i++) {
+//   console.log(ranks)
+// }
 
+for (let i = 0; i < suits.length; i++) {
+    for (let r = 0; r < ranks.length; r++) {
+      // console.log(ranks[r] + suits[i])
+      deck.push(ranks[r] + suits[i])
+    }
+  }
+  console.log(deck)
