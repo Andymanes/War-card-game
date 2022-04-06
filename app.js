15399e986d19479db654d1b42ae0f5ca3a914b67
Andymanes <101294232+Andymanes@users.noreply.github.com>
2022-04-06 11:40:59 -0400

    step 1 & 2 psuedo code

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 /*
-!!!! STEP ONE!!!!
+!!!! STEP ONE !!!!
 - Will need to make a deck of cards
     - all 4 suits
     - all 13 values for each suit
         - thinking an array for each maybe?
 
-!!!! STEP TWO!!!!
+!!!! STEP TWO !!!!
 - Find a way to shuffle the deck
-    
+    - thinking a for loop with the suits and values arrays but dont
+      know what that will be yet
+        - Math.floor(Math.random()).... something probably
+
+!!!! STEP 3 !!!!
+- Assign half of the shuffled deck to player 1 and the other half 
+  to player 2
+    - probably want some kind of a function called "deal" that will do this
+      that can be called on later to restart the game
 
 
 
