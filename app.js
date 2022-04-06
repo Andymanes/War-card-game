722eb68ae7ff65e2a3c22eeb1941990ef5ce37be
Andymanes <101294232+Andymanes@users.noreply.github.com>
2022-04-06 11:31:52 -0400

    step one psuedo code

---
+++ app.js
@@ -0,0 +1,21 @@
+/*
+!!!! STEP ONE!!!!
+- Will need to make a deck of cards
+    - all 4 suits
+    - all 13 values for each suit
+        - thinking an array for each maybe?
+
+!!!! STEP TWO!!!!
+- Find a way to shuffle the deck
+    
+
+
+
+
+
+
+
+
+*/
+
+
