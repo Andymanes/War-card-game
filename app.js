411e48a61f6acbf97c7c5d12cc62bd371f16ca7f
Andymanes <101294232+Andymanes@users.noreply.github.com>
2022-04-06 22:10:12 -0400

    basic html setup for game table

--- app.js
+++ app.js
@@ -10,14 +10,20 @@
 //   console.log(ranks)
 // }
 
+// GLOBAL VARIABLES
+
+
 let suits = ['S', 'C', 'H', 'D']
 let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 let deck = []
+
+
 
 for (let i = 0; i < suits.length; i++) {
      for (let r = 0; r < ranks.length; r++) {
         // console.log(ranks[r] + suits[i])
     deck.push(ranks[r] + suits[i])
+    // applying one of each suit to one of each rank
         }
 }
 
@@ -31,12 +37,16 @@ function shuffleDeck(array) {
         // this is the Fisher-Yates-Algorithm
     }
 }
-  shuffleDeck(deck)
-  console.log(deck)
 
-  let cutDeck = Math.ceil(deck.length / 2)
+shuffleDeck(deck)
+console.log(deck)
+
+let cutDeck = Math.ceil(deck.length / 2)
 //   console.log(cutDeck)
-let topDeck = deck.splice(0, cutDeck)
+
+let topDeck = deck.slice().splice(0, cutDeck)
 console.log(topDeck)
-let bottomDeck = deck.splice(-cutDeck)
+
+let bottomDeck = deck.slice().splice(-cutDeck)
 console.log(bottomDeck)
+
